5afacd35b1c25b54f715af17a0325d9849095f11
Lucas Dal Pra Brascher <153088537+DalPra0@users.noreply.github.com>
2024-10-10 10:18:06 -0300

    Update treinarDeVolta.py

--- treinarDeVolta.py
+++ treinarDeVolta.py
@@ -1,14 +1,11 @@
 from transformers import Trainer, TrainingArguments, AutoModelForQuestionAnswering, AutoTokenizer
 from datasets import load_dataset
 
-# Carregar o dataset de perguntas e respostas
 dataset = load_dataset('json', data_files='perguntas_respostas_magic.json')
 
-# Carregar o modelo e tokenizer
 tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 model = AutoModelForQuestionAnswering.from_pretrained('bert-base-uncased')
 
-# Definir argumentos para o treinamento
 training_args = TrainingArguments(
     output_dir='./results',
     num_train_epochs=3,
@@ -17,15 +14,12 @@ training_args = TrainingArguments(
     save_total_limit=2,
 )
 
-# Treinador
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=dataset['train'],
 )
 
-# Treinar o modelo
 trainer.train()
 
-# Salvar o modelo
 trainer.save_model('./results')
